52415b2f2af09a552b4fa55acb26aee42e413525
Vishnu R <vishnu6364748848@gmail.com>
2026-02-24 19:34:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+});
+
+let h1 = document.querySelector("h1");
+h1.addEventListener("mouseenter", () => {
+    cursor.style.height = 143 + "px";
+    cursor.style.width = 143 + "px";
+    cursor.style.opacity = ".8";
+});
+
+h1.addEventListener("mouseleave", () => {
+    cursor.style.height = 50 + "px";
+    cursor.style.width = 50 + "px";
+    cursor.style.opacity = "1";
+});
